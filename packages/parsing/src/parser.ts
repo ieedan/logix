ff2345de3495e16eb4cbce0b9fc7bdf81b28ae1e
Aidan Bleser <aidanbleser35@gmail.com>
2024-10-18 14:51:47 -0500

    add exports + fix

--- packages/parsing/src/parser.ts
+++ packages/parsing/src/parser.ts
@@ -1,4 +1,4 @@
-import { Err, Ok, type Result } from "../../utils/src/index.ts";
+import { Err, Ok, type Result } from "@utils";
 import { EXPRESSION_KEYWORDS, type Token } from "./tokens.ts";
 import type { And, Branch, Rung } from "./logic.ts";
 import type { Expression } from "./expressions.ts";
